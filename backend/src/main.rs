98eae0b347321c575c016cd8d8635d32a20fa9f6
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-21 23:30:49 +0545

    added base server and deps

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused_imports)]
+use actix_web::get;
+use actix_web::{self, App, HttpServer, Responder, web};
+
+#[get("/")]
+pub async fn index() -> impl Responder {
+    "Hello, world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting server at http://127.0.0.1:8080");
+    HttpServer::new(move || App::new().service(index))
+        // why is copilot not working right now
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
